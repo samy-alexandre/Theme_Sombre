6cc3529a423616363dab192512e6bf8277b54321
samy-alexandre <samy.alexandre85@gmail.com>
2022-05-01 15:01:31 +0200

    first commit

--- Exemple.js
+++ Exemple.js
@@ -1,117 +1,24 @@
-class Personnage {
-  constructor(pseudo, classe, sante, attaque) {
-    this.pseudo = pseudo;
-    this.classe = classe;
-    this.sante = sante;
-    this.attaque = attaque;
-    this.niveau = 1;
-  }
+let button = document.querySelector("#mode");
+let span = document.querySelector("span");
 
-  get informations() {
-    return (
-      this.pseudo +
-      " " +
-      this.classe +
-      " a " +
-      this.sante +
-      " point de vie et est au niveau " +
-      this.niveau
-    );
-  }
-
-  evoluer() {
-    this.niveau += 1;
-    console.log(this.pseudo + " passe au niveau " + this.niveau + " ! ");
-  }
-
-  verifierSante() {
-    if (this.sante <= 0) {
-      this.sante = 0;
-      console.log(this.pseudo + " a perdu ! bouhh");
+if (localStorage.getItem("theme")) {
+    if (localStorage.getItem("theme") == "sombre") {
+        modeSombre();
     }
-  }
 }
 
-class Magicien extends Personnage {
-  constructor(pseudo) {
-    super(pseudo, "magicien", 170, 90);
-  }
+button.addEventListener("click", () => {
+    if (document.body.classList.contains("dark")) {
+        document.body.className = "";
+        span.textContent = "Thème sombre";
+        localStorage.setItem("theme", "claire");
+    } else {
+        modeSombre();
+    }
+});
 
-  attaquer(personnage) {
-    personnage.sante -= this.attaque;
-
-    console.log(
-      this.pseudo +
-        " attaque " +
-        personnage.pseudo +
-        " en lancant un sort ( " +
-        this.attaque +
-        " degats )"
-    );
-    this.evoluer();
-    personnage.verifierSante();
-  }
-
-  coupSpecial(personnage) {
-    personnage.sante -= this.attaque * 5;
-    console.log(
-      this.pseudo +
-        " attaque " +
-        personnage.pseudo +
-        " en lancant un coup special ( " +
-        this.attaque * 5 +
-        " degats )"
-    );
-
-    this.evoluer();
-    personnage.verifierSante();
-  }
+function modeSombre() {
+    document.body.className = "dark";
+    span.textContent = "Thème claire";
+    localStorage.setItem("theme", "sombre");
 }
-
-// var personnage1 = new Magicien("samsampanda");
-// console.log(personnage1);
-
-class Guerrier extends Personnage {
-  constructor(pseudo) {
-    super(pseudo, "guerrier", 350, 50);
-  }
-
-  attaquer(personnage) {
-    personnage.sante -= this.attaque;
-    console.log(
-      this.pseudo +
-        " attaque " +
-        personnage.pseudo +
-        " en lancant un sort ( " +
-        this.attaque +
-        " degats )"
-    );
-    this.evoluer();
-    personnage.verifierSante();
-  }
-
-  coupSpecial(personnage) {
-    personnage.sante -= this.attaque * 5;
-    console.log(
-      this.pseudo +
-        " attaque " +
-        personnage.pseudo +
-        " en lancant un coup special ( " +
-        this.attaque * 5 +
-        " degats )"
-    );
-
-    this.evoluer();
-    personnage.verifierSante();
-  }
-}
-
-var gandalf = new Magicien("Gandalf");
-var thor = new Guerrier("Thor");
-console.log(thor.informations);
-console.log(gandalf.informations);
-gandalf.attaquer(thor);
-console.log(thor.informations);
-thor.attaquer(gandalf);
-console.log(gandalf.informations);
-thor.coupSpecial(gandalf);
